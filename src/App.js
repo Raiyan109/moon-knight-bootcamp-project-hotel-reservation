32e37515d68f9ad646f704e77ede5c1410bd4ae3
Raiyan109 <kraiyan109@gmail.com>
2022-09-16 10:25:05 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Shared/Navbar/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import styles from './Home.module.css'
+import MovingComponent from 'react-moving-text'
+import KingBedIcon from '@mui/icons-material/KingBed';
+
 const Home = () => {
     return (
-        <div>
+        <div className={styles.home}>
+            <div className={styles.homeWrapper}>
+                <div className={styles.upperBanner}>
+                    <h1 className={styles.animatedBannerText}><MovingComponent
+                        type="typewriter"
+                        dataText={[
+                            'Find your next stay',
+                            'Search  hotels'
+                        ]} /></h1>
+                </div>
 
+                <div className={styles.bottomBanner}>
+                    <div className={styles.bottomBannerInputWrapper}>
+                        <span className={styles.bottomBannerInputIcon}>
+                            <KingBedIcon />
+                        </span>
+                        <input type="text" className={styles.bottomBannerInput}
+                            placeholder='Where are you going?'
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
